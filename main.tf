cf46aaeefedaab21faab1e66869b8c4cd467b6bb
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-23 17:54:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,6 @@ resource "ibm_pi_network" "my_subnet" {
   pi_cidr		= "10.1.0.0/24"
   pi_gateway  = "10.1.0.1"
   pi_dns = ["8.8.8.8"]
-  pi_ip_address_range  = "10.0.0.10-10.1.0.250"
 }
 
 data "ibm_pi_public_network" "my_subnet" {
