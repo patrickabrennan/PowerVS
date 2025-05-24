3a167f7631e83cb53082ac93ecce034d7506750d
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-24 14:18:03 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,31 @@
+########################################################
+# Mandatory variables
+########################################################
+
+variable "pi_zone" {
+  description = "IBM Cloud PowerVS zone."
+  type        = string
+  validation {
+    condition     = contains(["syd04", "syd05", "eu-de-1", "eu-de-2", "lon04", "lon06", "us-east", "wdc06", "wdc07", "us-south", "dal10", "dal12", "tok04", "osa21", "sao01", "sao04", "mon01", "tor01", "mad02", "mad04", "dal14"], var.pi_zone)
+    error_message = "Only Following DC values are supported : syd04, syd05, eu-de-1, eu-de-2, lon04, lon06, us-east, wdc06, wdc07, us-south, dal10, dal12, tok04, osa21, sao01, sao04, mon01, tor01, mad02, mad04, dal14"
+  }
+}
+
+variable "pi_resource_group_name" {
+  description = "Existing Resource Group Name."
+  type        = string
+}
+
+variable "pi_workspace_name" {
+  description = "Name of IBM Cloud PowerVS workspace which will be created."
+  type        = string
+}
+
+variable "pi_ssh_public_key" {
+  description = "Name and value of the Public SSH key to create in PowerVS workspace."
+  type = object({
+    name  = string
+    value = string
+  })
+}
 
