992f2fe4d8462dfe6bcbd3520750cd3e6fcfe27e
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-24 17:38:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,9 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version => "1.57.0"
+      version = "1.57.0"
     }
   }
-  required_version = "1.12.1"
 }
 
 provider "ibm" {
