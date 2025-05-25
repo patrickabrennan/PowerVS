acbbca9c76025480ee3156e7faaaf046f64a0923
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-25 14:56:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,14 +59,18 @@ resource "ibm_pi_volume_attach" "test_volume" {
   pi_instance_id = ibm_pi_instance.my_instance.instance_id
 }
 
-#resource "ibm_powervs_ssh_key" "PowerVS_sshkey" {
-#  name       = "powervs-ssh"
-#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZUw1yvE8A15vPk48W637EjMi/xAtugZJRyxHzmNvcsPRgkJ2ox7owgf3vJNC20yzcArV83uPZnec7lfjfWggVBpI/VETgaeeGC1UB6ilu0WO6MPD5BpVhg5HknMXtaVfmQHdG3Ycw0Ilg8DGFWjTRneTV7mpu00TYQZELBrShE9iVG5RCVQ3Fka8xt9wnCVYj/Qjo4VQyfi36zJe47/XH/Oji2ANVijpPMKHPYQizrm0t/WTdzy2iSFUJhHRqOjjQx79KTWIks2ig3jSFguzztwYKmxDRbb7M7AHS1qutVr5MSeJSxtneNYLYgxwKOx5el0zXIqD/a4ow4TlZJDjStnTFg+RaHXJ4E8sJ6zWEmIlisjKgVPpud1MPkUxRO7kuxiZ37/TxaTkVLDGWylTtNAdQj+ih2h+FtPtHE3VJkOIAI3FTX1GSEdTQoH5eEs/xgLYCIg4ANcSEOoyaJqVgFnQInmXuXd0Hq391AMcOmWugPCioVHcJeanSSeQxw0M= sap"
-#}
+resource "ibm_is_ssh_key" "PowerVS_sshkey" {
+  name       = "powervs-ssh"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZUw1yvE8A15vPk48W637EjMi/xAtugZJRyxHzmNvcsPRgkJ2ox7owgf3vJNC20yzcArV83uPZnec7lfjfWggVBpI/VETgaeeGC1UB6ilu0WO6MPD5BpVhg5HknMXtaVfmQHdG3Ycw0Ilg8DGFWjTRneTV7mpu00TYQZELBrShE9iVG5RCVQ3Fka8xt9wnCVYj/Qjo4VQyfi36zJe47/XH/Oji2ANVijpPMKHPYQizrm0t/WTdzy2iSFUJhHRqOjjQx79KTWIks2ig3jSFguzztwYKmxDRbb7M7AHS1qutVr5MSeJSxtneNYLYgxwKOx5el0zXIqD/a4ow4TlZJDjStnTFg+RaHXJ4E8sJ6zWEmIlisjKgVPpud1MPkUxRO7kuxiZ37/TxaTkVLDGWylTtNAdQj+ih2h+FtPtHE3VJkOIAI3FTX1GSEdTQoH5eEs/xgLYCIg4ANcSEOoyaJqVgFnQInmXuXd0Hq391AMcOmWugPCioVHcJeanSSeQxw0M= sap"
+  crn = "crn:v1:bluemix:public:power-iaas:us-south:a/9568b7d56c7f48399bf8580557b5f022:10f21edf-47a3-454f-b2a3-e1032b4ea6c5:pvm-instance:01f8706c-8589-4f1f-b13b-ae5b67bd80d0"
+}
+
 
 #resource "ibm_is_floating_ip" "fip1" {
 #  name = "fip1"
 #  target = ibm_pi_instance.my_instance.network_id
+#  crn = "crn:v1:bluemix:public:power-iaas:us-south:a/9568b7d56c7f48399bf8580557b5f022:10f21edf-47a3-454f-b2a3-e1032b4ea6c5:pvm-instance:01f8706c-8589-4f1f-b13b-ae5b67bd80d0"
+}
 #}
 
 #output "sshcommand" {
