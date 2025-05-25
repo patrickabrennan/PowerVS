6866fc1a0ed9f66298b765db38e3ad660eaedc89
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-25 13:20:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "ibm_pi_volume_attach" "test_volume" {
   pi_instance_id = ibm_pi_instance.my_instance.instance_id
 }
 
-resource "ibm_is_ssh_key" "testacc_sshkey" {
+resource "ibm_powervs_ssh_key" "PowerVS_sshkey" {
   name       = "powervs-ssh"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZUw1yvE8A15vPk48W637EjMi/xAtugZJRyxHzmNvcsPRgkJ2ox7owgf3vJNC20yzcArV83uPZnec7lfjfWggVBpI/VETgaeeGC1UB6ilu0WO6MPD5BpVhg5HknMXtaVfmQHdG3Ycw0Ilg8DGFWjTRneTV7mpu00TYQZELBrShE9iVG5RCVQ3Fka8xt9wnCVYj/Qjo4VQyfi36zJe47/XH/Oji2ANVijpPMKHPYQizrm0t/WTdzy2iSFUJhHRqOjjQx79KTWIks2ig3jSFguzztwYKmxDRbb7M7AHS1qutVr5MSeJSxtneNYLYgxwKOx5el0zXIqD/a4ow4TlZJDjStnTFg+RaHXJ4E8sJ6zWEmIlisjKgVPpud1MPkUxRO7kuxiZ37/TxaTkVLDGWylTtNAdQj+ih2h+FtPtHE3VJkOIAI3FTX1GSEdTQoH5eEs/xgLYCIg4ANcSEOoyaJqVgFnQInmXuXd0Hq391AMcOmWugPCioVHcJeanSSeQxw0M= sap"
 }
