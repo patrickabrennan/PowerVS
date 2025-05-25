b786b4bc895ceab080796922715c700cee604fba
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-24 21:49:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "ibm" {
 
 data "ibm_pi_cloud_connection" "example" {
   pi_cloud_connection_name  = "test_cloud_connection"
-  pi_cloud_instance_id      = "08d4d95c-2597-4882-92d6-2b3516cc0fd9"
+  pi_cloud_instance_id      = "a/9568b7d56c7f48399bf8580557b5f022"
 }
 
 #output "pi_cloud_instance_id" {
