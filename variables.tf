25ec097915ce33ed523c389b5445d03eb64f2b33
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-25 15:58:53 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,71 @@
+variable "ibmcloud_api_key" {
+  type		= string
+}
+
+variable "region" {
+  type		= string
+}
+
+variable "pi_cloud_instance_id" {
+  type		= string
+}
+
+variable "pi_network_name" {
+  type		= string
+}
+
+variable "pi_network_type" {
+  type		= string
+}
+
+variable "pi_memory" {
+  type		= string
+}
+
+variable "pi_processors" {
+  type		= string
+}
+
+variable "pi_instance_name" {
+  type		= string
+}
+
+variable "pi_proc_type" {
+  type		= string
+}
+
+variable "pi_image_id" {
+  type		= string
+}
+
+variable "pi_sys_type" {
+  type		= string
+}
+
+variable "pi_cloud_instance_id" {
+  type		= string
+}
+
+variable "pi_volume_size" {
+  type		= string
+}
+
+variable "pi_volume_name" {
+  type		= string
+}
+
+variable "pi_volume_type" {
+  type		= string
+}
+
+variable "pi_key_name" {
+  type		= string
+}
+
+variable "pi_ssh_key" {
+  type		= string
+}
+
+  
+  
 
