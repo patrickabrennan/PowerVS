72e6336e7361ae8a84beed8f92e617d6f3aa07f9
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-26 21:35:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "ibm_resource_group" "group" {
 }
 
 data "ibm_resource_instance" "powervs" {
-  name = "my_instance"
+  name = "DNS Services-ip"
   service = "power-iaas"
   location = var.region
   resource_group_id = be53b0ed877e454ca8cae5384ef59362
