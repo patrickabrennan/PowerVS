db21ae70e746411baa728bd22070e8494fd5137e
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-26 21:42:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ data "ibm_resource_instance" "powervs" {
   name = var.workspace-name
   service = "power-iaas"
   location = var.region
-  resource_group_id = be53b0ed877e454ca8cae5384ef59362
 }
 
 output "pi_cloud_instance_id" {
